a82d7c0b644f3ea2c822c0391cb8eabe03ee9475
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-07-05 17:45:39 +0200

    form-submit

---
+++ formScript.js
@@ -0,0 +1,63 @@
+const firstTimeForm = document.querySelector('.first-time-form');
+const FIRST_NAME_KEY = 'first-name';
+const LAST_NAME_KEY = 'last-name';
+function setCookie(key, value, exp) {
+    document.cookie = `${key}=${value};expires=${exp};path=/`;
+}
+
+function getCookie(key) {
+    const cookies = document.cookie.split(';');
+    const cookie = cookies.find(cookie => cookie.includes(key));
+    if (cookie) {
+        return cookie.split('=')[1];
+    }
+    return null;
+}
+
+function hideFirstTimeForm() {
+    firstTimeForm.classList.add('first-time-form--hidden');
+}
+
+function showFirstTimeForm() {
+    firstTimeForm.classList.remove('first-time-form--hidden');
+}
+
+function showWelcomeMessage(firstName, lastName) {
+    const welcomeMessage = document.createElement('h1');
+    welcomeMessage.textContent = `Welcome ${firstName} ${lastName}`;
+    document.body.querySelector(".main-content").prepend(welcomeMessage);
+}
+
+function hideWelcomeMessage() {
+    const welcomeMessage = document.querySelector('h1');
+    if (welcomeMessage) {
+        welcomeMessage.remove();
+    }
+}
+
+function checkIfLoggedIn() {
+    const firstName = getCookie(FIRST_NAME_KEY);
+    const lastName = getCookie(LAST_NAME_KEY);
+    if (firstName && lastName) {
+        hideFirstTimeForm();
+        showWelcomeMessage(firstName, lastName);
+    }
+
+}
+
+function handleFirstTimeFormSubmit(event) {
+    event.preventDefault();
+
+    const fields = event.target.elements;
+    const firstName = fields.firstName.value;
+    const lastName = fields.lastName.value;
+
+    setCookie(FIRST_NAME_KEY, firstName, 'Fri, 31 Dec 2023 23:59:59 GMT')
+    setCookie(LAST_NAME_KEY, lastName, 'Fri, 31 Dec 2023 23:59:59 GMT')
+    hideFirstTimeForm();
+    showWelcomeMessage(firstName, lastName);
+}
+
+firstTimeForm.addEventListener('submit', handleFirstTimeFormSubmit);
+
+document.addEventListener('DOMContentLoaded', checkIfLoggedIn);
